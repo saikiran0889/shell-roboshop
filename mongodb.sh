66da1ddce0111a89730351f9369180024554f578
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-25 21:15:56 +0530

    est

--- mongodb.sh
+++ mongodb.sh
@@ -31,6 +31,12 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "adding mongo repo"
 
 dnf install mongodb-org -y &>>LOG_FILE
-
+VALIDATE $? "installing mongodb"
 systemctl enable mongod
+VALIDATE $? "enabling mongodb"
 systemctl start mongod  
+VALIDATE $? "Started mongodb"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.config
+VALIDATE $? "allowing remote connections mongodb"
+systemctl restart mongod
+VALIDATE $? "instalrestart mongodb"
