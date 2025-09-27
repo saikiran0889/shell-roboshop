49b43a5b5a1c5f23061a1b8e7f798535ad410cec
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-27 16:25:54 +0530

    est

--- redis.sh
+++ redis.sh
@@ -31,17 +31,17 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 } 
 
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Redis disabled"
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Redis enabled"
-dnf install redis -y 
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Redis installed"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 
-VALIDATE $? "changed in config file"
+VALIDATE $? "changed in config file" &>>$LOG_FILE
 systemctl enable redis 
 VALIDATE $? "Redis eabled"
-systemctl start redis 
+systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Redis start"
 
