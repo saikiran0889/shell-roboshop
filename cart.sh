f66f6c847527b175b81a6edc778445f18c176d25
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-27 23:41:53 +0530

    est

---
+++ cart.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -euo pipefail
+trap 'echo "this is error messag $LINENO, command is $BASH_COMMAND"'  ERR
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+SCRIPT_DIR=$PWD
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+} 
+
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disable nodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enable nodeJS"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installed nodeJS"
+if [ $? -ne 0 ]; then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+else
+echo "user already exist"
+fi
+mkdir -p /app 
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app 
+unzip -o /tmp/cart.zip &>>$LOG_FILE
+npm install &>>$LOG_FILE
+VALIDATE $? "Install packages"
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+
+systemctl daemon-reload
+systemctl enable cart 
+VALIDATE $? "enabled cart service"
+systemctl start cart
+VALIDATE $? "start cart service"
