24fa146d00ac7ac354b148adbf28aa1f587a3c17
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-27 22:28:42 +0530

    est

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -12,7 +12,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
-SCRIPT_DIR=$(PWD)
+SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
